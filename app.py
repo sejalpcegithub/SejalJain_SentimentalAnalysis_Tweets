40462d4842b373182e8f069eedfa1b534bd3b0e8
sejalpcegithub <101696920+sejalpcegithub@users.noreply.github.com>
2022-08-10 21:47:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+  
+    return render_template("index1.html")
+
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
+
+
+@app.route('/index3')
+def index3():
+    return render_template('index3.html')
+
+
+@app.route('/index4',methods=['GET'])
+def index4():
+    prediction="Neutral"
+    return render_template('index4.html', prediction_text='predicted sentimental analysis for given dataset is : {}'.format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
+    
